863f21e003723493fe818d17098d2d64690460bf
Nakib <nbmomin14@gmail.com>
2021-11-24 14:44:51 -0500

    cleanup

--- coffee-shops/pages/index.js
+++ coffee-shops/pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
       <main className={styles.main}>
         <h1 className={styles.title}>Cofee Shops</h1>
       </main>
-      <footer className={styles.footer}></footer>
     </div>
   )
 }
+ 
