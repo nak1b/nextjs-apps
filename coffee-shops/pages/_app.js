3b9c5db15fea67740e6a04e5f1a6ee336eef6265
Nakib <nbmomin14@gmail.com>
2021-11-25 10:25:49 -0500

    added fonts and more styles

--- coffee-shops/pages/_app.js
+++ coffee-shops/pages/_app.js
@@ -1,6 +1,4 @@
-import Image from 'next/image'
 import '../styles/globals.css'
-import styles from '../styles/Home.module.css'
 
 function MyApp({ Component, pageProps }) {
   return (
--- coffee-shops/pages/index.js
+++ coffee-shops/pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Banner from '../components/banner'
 
@@ -15,6 +16,9 @@ export default function Home() {
 
       <main className={styles.main}>
         <Banner handleOnClick={handleBannerButtonClick} buttonText="View Stores nearby" />
+        <div className={styles.heroImage}>
+          <Image alt="hero image" src="/static/hero-image.png" height={400} width={700} />
+        </div>
       </main>
     </div>
   );
